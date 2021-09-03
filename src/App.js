ad498a7764df616016c312f1f3282d243a20e8df
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2021-09-03 12:44:37 -0400

    Départ

--- src/App.js
+++ src/App.js
@@ -11,73 +11,10 @@ export default class App {
 		app = document.getElementById("app");
 
 		// PREMIERE ETAPE
-		table = document.createElement("table");
-		app.appendChild(table);
-		table.setAttribute("border", "1");
-		tr = document.createElement("tr");
-		table.appendChild(tr);
-		th = document.createElement("th");
-		tr.appendChild(th);
-		th.innerHTML = "&times;";
-		th = document.createElement("th");
-		tr.appendChild(th);
-		th.innerHTML = num;
-		for(let i = 1; i <= 12; i += 1) {
-			tr = document.createElement("tr");
-			table.appendChild(tr);
-			th = document.createElement("th");
-			tr.appendChild(th);
-			th.innerHTML = i;
-			td = document.createElement("td");
-			tr.appendChild(td);
-			td.innerHTML = i * num;
-		}
 
-		//DEUXIEME ETAPE
-		table = app.appendChild(document.createElement("table"));
-		table.setAttribute("border", "1")
-		tr = table.appendChild(document.createElement("tr"));
-		th = tr.appendChild(document.createElement("th"));
-		th.innerHTML = "&times;";
-		
-		for (let i = 1; i <= 12; i += 1) {
-			th = tr.appendChild(document.createElement("th"));
-			th.innerHTML = i;
+		// DEUXIEME ETAPE
 
-		}
-
-		tr = table.appendChild(document.createElement("tr"));
-		th = tr.appendChild(document.createElement("th"));
-		th.innerHTML = num;
-		
-		for (let i = 1; i <= 12; i += 1) {
-			td = tr.appendChild(document.createElement("td"));
-			td.innerHTML = i * num;
-		}
-
-		//TROISIEME ETAPE
-		table = app.appendChild(document.createElement("table"));
-		table.setAttribute("border", "1")
-		tr = table.appendChild(document.createElement("tr"));
-		th = tr.appendChild(document.createElement("th"));
-		th.innerHTML = "&times;";
-		
-		for (let i = 1; i <= 12; i += 1) {
-			th = tr.appendChild(document.createElement("th"));
-			th.innerHTML = i;
-
-		}
-
-		for (let j = 1; j <= 12; j += 1) {
-			tr = table.appendChild(document.createElement("tr"));
-			th = tr.appendChild(document.createElement("th"));
-			th.innerHTML = j;
-		
-			for (let i = 1; i <= 12; i += 1) {
-				td = tr.appendChild(document.createElement("td"));
-				td.innerHTML = i * j;
-			}
-		}
+		// TROISIEME ETAPE
 
 	}
 	/**
